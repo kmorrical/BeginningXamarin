4953eae398b587d65d41e7223b161d937eb9fb79
kmorrical <kathryn.morrical@gmail.com>
2017-03-05 10:31:13 -0800

    first commit

---
+++ ContentPageLearning/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Xamarin.Forms;
+
+namespace ContentPageLearning
+{
+	public partial class App : Application
+	{
+		public App()
+		{
+			InitializeComponent();
+
+			MainPage = new Page1();
+			//changing newPage1 makes it the starting point for doing any of the work
+		}
+
+		protected override void OnStart()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
